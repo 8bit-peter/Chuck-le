aaeac44d5540feb1ef771a16bda103a44dd09bac
Piotr Jaczewski <pp.jaczewski@gmail.com>
2018-05-17 11:27:39 +0200

    First commit - working skeleton

---
+++ js/app.js
@@ -0,0 +1,13 @@
+$( () => {
+    
+    const urlApi  = 'https://api.chucknorris.io/jokes/random';
+
+    $.ajax ({
+        url : urlApi,
+        dataType : 'json'
+    }).done((e) => {
+        $('img').attr('src',e.icon_url);
+        $('.jokeContent').text(e.value);
+    });
+
+})
