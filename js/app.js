56c7bc945c9c54bd2aa957132ce1ad984b63b37d
Piotr Jaczewski <pp.jaczewski@gmail.com>
2018-06-06 11:16:04 +0200

    Work in progress

--- js/app.js
+++ js/app.js
@@ -10,5 +10,21 @@ $( () => {
         $('.jokeContent').text(e.value);
     });
 
+    var rateValue = $('#jokeRate');
+    var form = $('#myForm');
+    var checkedRate = form.find('input[name=rating]:checked', '#myForm').val();
+
+    $('#myForm').submit(function(e) {
+        e.preventDefault();
+        console.log($('input[name=rating]:checked', '#myForm').val());
+    })
+
+    // $('#myForm input').on('change', function() {
+    //     alert($('input[name=rating]:checked', '#myForm').val()); 
+    //  });
+
+    //  $('#myForm input').on('change', function() {
+    //     alert($('input[name=rating]:checked', '#myForm').val()); 
+    //  });
     
 })
