cdf017913b389609ad9f224e49438b8307c77daf
Piotr Jaczewski <pp.jaczewski@gmail.com>
2018-06-11 12:46:03 +0200

    Joke rating working

--- js/app.js
+++ js/app.js
@@ -7,17 +7,19 @@ $( () => {
     var checkedRate = form.find('input[name=rating]:checked', '#myForm').val();
     var jokeRates = [];
     var jokeAvg = 0;
+    var jokeAvgFixed = 0;
 
-    // test
-    var jokeSum = 0;
+    function getJoke() {
+        $.ajax ({
+            url : urlApi,
+            dataType : 'json'
+        }).done((e) => {
+            $('img').attr('src',e.icon_url);
+            $('.jokeContent').text(e.value);
+        });
+    };
 
-    $.ajax ({
-        url : urlApi,
-        dataType : 'json'
-    }).done((e) => {
-        $('img').attr('src',e.icon_url);
-        $('.jokeContent').text(e.value);
-    });
+    getJoke();
 
     $('#myForm').submit(function(e) {
         e.preventDefault();
@@ -25,14 +27,23 @@ $( () => {
         jokeAvg = jokeRates.reduce(function(a, b) {
             return (parseInt(a) + parseInt(b))
         }) / jokeRates.length;
-        console.log("---NEXT SUBMIT---")
-        console.log("Joke Avg "+jokeAvg);
-        console.log("Joke Rates "+jokeRates)
-        console.log("Joke length "+jokeRates.length);
-        jokeSum = jokeRates.reduce(function(a, b) {
-            return parseInt(a) + parseInt(b)
+
+        jokeAvgFixed = jokeAvg.toFixed(2);
+
+        rateValue.text(jokeAvgFixed);
+
+        $('input[name=rating]:checked').each(function(){
+            $(this).prop('checked', false);
         });
-        console.log("Joke SUm "+jokeSum)
+
+        getJoke();
+
     });
     
 });
+
+
+
+    
+
+
